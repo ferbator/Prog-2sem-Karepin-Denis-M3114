f022933f0a872da6972d0647ef67afa5e0e04823
ferbator <75217990+ferbator@users.noreply.github.com>
2021-05-05 13:37:08 +0300

    Update main.cpp

--- lab4/main.cpp
+++ lab4/main.cpp
@@ -4,6 +4,19 @@
 
 using namespace std;
 
+//место для предикатов
+/*
+    equal_to бинарный предикат равенства
+    not_equal_to бинарный предикат неравенства
+    greater бинарный предикат >
+    less бинарный предикат < (используется по умолчанию)
+    greater_equal бинарный предикат >=
+    less_equal бинарный предикат <=
+    logical_and бинарный предикат И
+    logical_or бинарный предикат ИЛИ
+    logical_not унарный предикат НЕ
+ */
+
 template<typename type>
 bool my_greater(type a, type b) {
     if (a > b)
@@ -11,15 +24,23 @@ bool my_greater(type a, type b) {
     return false;
 }
 
+template<typename type>
+bool my_less(type a, type b) {
+    if (a < b)
+        return true;
+    return false;
+}
+
+// место для алгоритмов
+
 template<class Iterator, class T>
 bool all_of(Iterator it_first, Iterator it_second, bool (&comp_func)(T, T)) {
     while (it_first != it_second) {
         it_first++;
         if (!comp_func(*prev(it_first), *it_first))
             return false;
-        return true;
     }
-    return false;
+    return true;
 }
 
 template<class Iterator, class T>
@@ -28,7 +49,6 @@ bool any_of(Iterator it_first, Iterator it_second, bool (&comp_func)(T, T)) {
         it_first++;
         if (comp_func(*prev(it_first), *it_first))
             return true;
-        return false;
     }
     return false;
 }
@@ -39,9 +59,9 @@ bool none_of(Iterator it_first, Iterator it_second, bool (&comp_func)(T, T)) {
         it_first++;
         if (comp_func(*prev(it_first), *it_first))
             return false;
-        return true;
+
     }
-    return false;
+    return true;
 }
 
 template<class Iterator, class T>
@@ -53,15 +73,27 @@ bool one_of(Iterator it_first, Iterator it_second, bool (&comp_func)(T, T)) {
             count++;
         if (count == 1)
             return true;
-        return false;
     }
     return false;
 }
 
+template<class Iterator, class T>
+bool is_sorted(Iterator it_first, Iterator it_second, bool (&comp_func)(T, T)) {
+    while (it_first != it_second) {
+        it_first++;
+        if (!comp_func(*prev(it_first), *it_first))
+            return false;
+    }
+    return true;
+}
+
 int main() {
+
     vector<int> vec = {1, 2, 3, 4};
 
-    cout << all_of(vec.begin(), vec.end(), my_greater<int>);
+    cout << is_sorted(vec.begin(), vec.end(), my_greater<int>) << "\n";
+
+    cout << is_sorted(vec.begin(), vec.end(), my_less<int>);
 
     return 0;
 }
